794fbf76ae899022ae4185922761e2fc5ac43db6
Dan <susuDan@users.noreply.github.com>
2015-01-11 13:37:49 -0800

    trying an edit in ep.php

--- ep.php
+++ ep.php
@@ -2,7 +2,7 @@
 
 ////// Setting up a test php server for mobile backend
 // $response[$cmd] = array('err' => 1 ....
-$response = array('err' => 1, 'text' => array("Hello?", "Fuck you!", "Yes?", "What do you want?"));
+$response = array('err' => 1, 'text' => array("Hello SuSu?", "Fuck you!", "Yes?", "What do you want?"));
 $result = json_encode($response);
 
 echo $result
